ea1ffce36824b51a6e63deca0c4cc0e0508a0477
rnantume <nnrobin37@gmail.com>
2018-08-29 12:10:36 +0300

    api fetches all questions

--- API/app.py
+++ API/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, Blueprint
+from flask_jwt import JWT
 
+
+from API.resources.auth import authenticate, identity
+from API.resources.users import users_bp
 
 app = Flask(__name__,instance_relative_config=True)
-
+app.secret_key = 'qwertyrvtimber'
 app.config['DEBUG'] = True
+jwt = JWT(app, authenticate, identity)
 
-from API.resources.users import users_bp
 
 """
 registering the users blueprint
