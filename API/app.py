51bb8a198edad50d4c87c81d82aba3f24a6c8ace
rnantume <nnrobin37@gmail.com>
2018-08-29 12:13:03 +0300

    fetch all questions

--- API/app.py
+++ API/app.py
@@ -4,16 +4,18 @@ from flask_jwt import JWT
 
 from API.resources.auth import authenticate, identity
 from API.resources.users import users_bp
+# from API.resources.questions import questions_bp
 
 app = Flask(__name__,instance_relative_config=True)
 app.secret_key = 'qwertyrvtimber'
 app.config['DEBUG'] = True
-jwt = JWT(app, authenticate, identity)
+
+jwt = JWT(app, authenticate, identity) #creates a default route /auth
 
 
 """
-registering the users blueprint
+registering the users, questions blueprint
 """
 app.register_blueprint(users_bp)
-
+# app.register_blueprint(questions_bp)
 
