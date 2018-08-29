cbf3f70a6d03d5b0f14f7dd7e9ef2eadce96b0f8
rnantume <nnrobin37@gmail.com>
2018-08-29 14:25:23 +0300

    adds absolute path to database.ini

--- API/app.py
+++ API/app.py
@@ -1,16 +1,10 @@
 from flask import Flask, Blueprint
-from flask_jwt import JWT
 
-
-from API.resources.auth import authenticate, identity
 from API.resources.users import users_bp
-# from API.resources.questions import questions_bp
+from API.resources.questions import questions_bp
 
 app = Flask(__name__,instance_relative_config=True)
-app.secret_key = 'qwertyrvtimber'
 app.config['DEBUG'] = True
-
-jwt = JWT(app, authenticate, identity) #creates a default route /auth
 
 
 """
