e0a6803addd8d92bad93109f55babf4f55e5a9d7
rnantume <nnrobin37@gmail.com>
2018-08-30 13:28:31 +0300

    tests API for user signup

--- tests/test_signup.py
+++ tests/test_signup.py
@@ -15,25 +15,74 @@ class UsersTestCase(unittest.TestCase):
         
         """setup test variables"""
         self.new_user = {'username':'robin','email':'nnrobin37@gmail.com',
-                            'password':'der1234'}
+                            'password':'G@mb13rs', 'verify_password':'G@mb13rs'}
+        self.another_user = {'username':'stella','email':'calyphstruck@gmail.com',
+                            'password':'G@mb13rs', 'verify_password':'G@mb13rs'}
         self.with_wrong_email = {'username':'robin','email':'robin',
-                            'password':'der1234'}
+                            'password':'G@mb13rs', 'verify_password':'G@mb13rs'}
+        self.with_wrong_password = {'username':'robin','email':'nnrobin37@gmail.com',
+                            'password':'1234', 'verify_password':'1234'}
+        self.with_no_match = {'username':'robin','email':'nnrobin37@gmail.com',
+                            'password':'G@mb13rs', 'verify_password':'1234'}                            
 
     def test_api_adds_new_user(self):
         """Test API can add new user to database(POST request)."""
         res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
                          data=json.dumps(self.new_user),content_type='application/json')
         self.assertEqual(res.status_code, 201)
-
+        
     def test_api_cannot_add_user_with_wrong_email(self):
         """Test API cannot add new user to database(POST request) if email is invalid."""
         res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
                          data=json.dumps(self.with_wrong_email),content_type='application/json')
         self.assertEqual(res.status_code, 400)
 
+    def test_api_cannot_post_user_if_password_is_wrong(self):
+        """Test API cannot add new user to database(POST request) if password is wrong."""
+        res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
+                         data=json.dumps(self.with_wrong_password),content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_api_cannot_post_user_if_password_and_verification_dont_match(self):
+        """Test API cannot add new user to database(POST request) if password 
+            and verify_password do not match"""
+        res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
+                          data=json.dumps(self.with_no_match),content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_api_cannot_post_user_if_username_already_exists(self):
+        """Test API cannot add new user to database(POST request) if password is wrong."""
+        res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
+                         data=json.dumps(self.new_user),content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+        res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
+                         data=json.dumps(self.new_user),content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_api_cannot_post_user_if_email_already_exists(self):
+        """Test API cannot add new user to database(POST request) if password is wrong."""
+        res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
+                         data=json.dumps(self.new_user),content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+        res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
+                         data=json.dumps(self.new_user),content_type='application/json')
+        self.assertEqual(res.status_code, 400)
+
+    def test_api_posts_user_with_different_credentila(self):
+        """Test API now posts new user with different credentila"""
+        res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
+                         data=json.dumps(self.new_user),content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+        res = self.client.post('/StackOverflow-lite/api/v1/auth/signup',
+                         data=json.dumps(self.another_user),content_type='application/json')
+        self.assertEqual(res.status_code, 201)
+
+
     def tearDown(self):
-        """teardown initialised variables"""
-        pass
+        """teardown table contents"""
+        query = """DROP TABLE users CASCADE"""
+        cur = connection()
+        cur.execute(query)
 
 if __name__ =='__main__':
     unittest.main()
